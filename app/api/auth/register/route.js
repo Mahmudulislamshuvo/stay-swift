27788828a29a6877a6d7b1ab2e22c5b55e30d835
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-23 00:16:22 +0600

    feat: add user registration endpoint with password hashing and response handling

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,34 @@
+import { dbConnect } from "@/lib/mongoDb";
+import { userModel } from "@/models/userModel";
+import { NextResponse } from "next/server";
+
+export const POST = async (request, _response) => {
+  const { fname, lname, email, password } = await request.json();
+  await dbConnect();
+
+  const hashPassword = await bcrypt.hash(password, 5);
+
+  const newUser = {
+    name: `${fname} ${lname}`,
+    email,
+    password: hashPassword,
+  };
+
+  try {
+    const createdUser = await userModel.create(newUser);
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "User registered successfully",
+        user: createdUser,
+      },
+      { status: 201 },
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, error: error.message },
+      { status: 500 },
+    );
+  }
+};
