7e4c228cb6761812f1ce4ca05a7e7ca4aa3e30bc
Mahmudul Islam Shuvo <mahmudulislammern@gmail.com>
2026-02-26 01:26:51 +0600

    feat: enhance Search component with input handling and validation for check-in/check-out dates

--- components/search/Search.jsx
+++ components/search/Search.jsx
@@ -1,4 +1,44 @@
-const Search = ({fromList}) => {
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
+
+const Search = ({ fromList }) => {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const pathname = usePathname();
+
+  const [serachTerm, setSearchTerm] = useState({
+    destination: "",
+    checkin: "",
+    checkout: "",
+  });
+
+  const [allowSearch, setAllowSearch] = useState(true);
+
+  const handleInputs = (e) => {
+    const { name, value } = e.target;
+
+    const updatedSearchTerm = {
+      ...serachTerm,
+      [name]: value,
+    };
+
+    if (
+      new Date(updatedSearchTerm.checkin) > new Date(updatedSearchTerm.checkout)
+    ) {
+      setAllowSearch(false);
+    } else {
+      setAllowSearch(true);
+    }
+
+    setSearchTerm(updatedSearchTerm);
+  };
+
+  const doSearch = () => {
+    const params = new URLSearchParams(serachTerm);
+  };
+
   return (
     <>
       <div className="lg:max-h-[250px] mt-6">
@@ -6,7 +46,11 @@ const Search = ({fromList}) => {
           <div>
             <span>Destination</span>
             <h4 className="mt-2">
-              <select name="destination" id="destination">
+              <select
+                onChange={handleInputs}
+                name="destination"
+                id="destination"
+              >
                 <option value="Bali">Bali</option>
                 <option value="Bali">Cox's Bazar</option>
                 <option value="Bali">Sylhet</option>
@@ -19,20 +63,40 @@ const Search = ({fromList}) => {
           <div>
             <span>Check in</span>
             <h4 className="mt-2">
-              <input type="date" name="checkin" id="checkin" />
+              <input
+                type="date"
+                name="checkin"
+                id="checkin"
+                onChange={handleInputs}
+              />
             </h4>
           </div>
 
           <div>
             <span>Checkout</span>
             <h4 className="mt-2">
-              <input type="date" name="checkout" id="checkout" />
+              <input
+                type="date"
+                name="checkout"
+                id="checkout"
+                onChange={handleInputs}
+              />
             </h4>
           </div>
         </div>
       </div>
 
-      <button className="search-btn">🔍️ {fromList ? "Modify Search": "Search"}</button>
+      <button
+        disabled={!allowSearch}
+        className={
+          allowSearch
+            ? "search-btn"
+            : "bg-gray-400 cursor-not-allowed search-btn"
+        }
+        onClick={doSearch}
+      >
+        🔍️ {fromList ? "Modify Search" : "Search"}
+      </button>
     </>
   );
 };
